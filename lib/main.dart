b30f7dafc2b3f86b8a1671dd73cc5152eed19876
The Chinh <chinh1998@gmail.com>
2020-04-14 02:05:19 +0700

    Update Button

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,25 @@ class MyApp extends StatelessWidget {
     children: <Widget>[
       Image.asset("assets/images/symbol@3x.png"),
       const SizedBox(height: 15.3),
-      RaisedButton(
-        onPressed: () {},
-        color: Colors.white,
-        padding: EdgeInsets.all(10.0),
-        child: Text(
-          '新規会員登録',
-          style: TextStyle(color: Color.fromRGBO(41, 182, 246, 10)),
+      ButtonTheme(
+        height: 18.7,
+        minWidth: 46.7,
+        child: RaisedButton(
+          onPressed: () {},
+          color: Colors.white,
+          padding: EdgeInsets.all(10.0),
+          child: Text(
+            '新規会員登録',
+            style: TextStyle(color: Color.fromRGBO(41, 182, 246, 10)),
+          ),
         ),
       ),
       const SizedBox(height: 8),
-      new GestureDetector(
+      new InkWell(
           onTap: () {},
           child: Text(
             'ログイン',
-            style: TextStyle(
-              fontSize: 10,
-              height: 5.3,
-              color: Colors.white
-            ),
+            style: TextStyle(fontSize: 15, color: Colors.white),
           ))
     ],
   );
