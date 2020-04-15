253da92beb99efb60f22864625a0644134ff393b
The Chinh <chinh1998@gmail.com>
2020-04-16 03:08:20 +0700

    Update Button

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Wash App",
-      home: Scaffold(
-          backgroundColor: Color.fromRGBO(41, 182, 246, 5),
-          body: Center(
-            child: middleColumn,
-          )),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromRGBO(41, 182, 246, 100),
+                Color.fromRGBO(0, 128, 203, 100)
+              ],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                middleColumn,
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
   Widget middleColumn = Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
     children: <Widget>[
-      Image.asset("assets/images/symbol@3x.png"),
+      Image.asset("assets/images/symbol@2x.png"),
       const SizedBox(height: 15.3),
       ButtonTheme(
         height: 18.7,
@@ -30,7 +54,10 @@ class MyApp extends StatelessWidget {
           padding: EdgeInsets.all(10.0),
           child: Text(
             '新規会員登録',
-            style: TextStyle(color: Color.fromRGBO(41, 182, 246, 10)),
+            style: TextStyle(
+              fontFamily: 'NotoSans',
+              color: Color.fromRGBO(41, 182, 246, 10),
+            ),
           ),
         ),
       ),
@@ -39,7 +66,10 @@ class MyApp extends StatelessWidget {
           onTap: () {},
           child: Text(
             'ログイン',
-            style: TextStyle(fontSize: 15, color: Colors.white),
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),
           ))
     ],
   );
