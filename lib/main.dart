79ca9570efe4b3f2326dc4ed211d1df7acb98247
The Chinh <chinh1998@gmail.com>
2020-04-17 12:39:38 +0700

    Update input section

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: RegisterPage(),
     );
   }
 }
@@ -32,7 +32,46 @@ class MyHomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                middleColumn,
+                new Column(
+                  children: <Widget>[
+                    Image.asset("assets/images/symbol@2x.png"),
+                    const SizedBox(height: 15.3),
+                    ButtonTheme(
+                      height: 18.7,
+                      minWidth: 46.7,
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ),
+                          );
+                        },
+                        color: Colors.white,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '新規会員登録',
+                          style: TextStyle(
+                            fontFamily: 'NotoSans',
+                            color: Color.fromRGBO(41, 182, 246, 10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    new InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'ログイン',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
@@ -40,37 +79,70 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget middleColumn = Column(
-    children: <Widget>[
-      Image.asset("assets/images/symbol@2x.png"),
-      const SizedBox(height: 15.3),
-      ButtonTheme(
-        height: 18.7,
-        minWidth: 46.7,
-        child: RaisedButton(
-          onPressed: () {},
-          color: Colors.white,
-          padding: EdgeInsets.all(10.0),
-          child: Text(
-            '新規会員登録',
-            style: TextStyle(
-              fontFamily: 'NotoSans',
-              color: Color.fromRGBO(41, 182, 246, 10),
-            ),
-          ),
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          padding: EdgeInsets.all(8),
+          children: <Widget>[
+            _buildInputSection(IconData(59516, fontFamily: 'MaterialIcons'),
+                'お好きなニックネーム', TextInputType.text, false),
+            _buildInputSection(IconData(57688, fontFamily: 'MaterialIcons'),
+                'Eメールアドレス', TextInputType.emailAddress, false),
+            _buildInputSection(IconData(59543, fontFamily: 'MaterialIcons'),
+                'パスワード', TextInputType.text, true),
+            _buildInputSection(IconData(59543, fontFamily: 'MaterialIcons'),
+                'パスワード', TextInputType.text, true)
+          ],
         ),
       ),
-      const SizedBox(height: 8),
-      new InkWell(
-          onTap: () {},
-          child: Text(
-            'ログイン',
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ))
-    ],
-  );
+    );
+  }
+
+  Card _buildInputSection(
+      IconData icon, String label, TextInputType type, bool checkPass) {
+    return Card(
+        color: Color.fromRGBO(228, 228, 228, 100),
+        child: Container(
+          padding: EdgeInsets.all(6),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 40,
+                width: 40,
+                child: Icon(
+                  icon,
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 230,
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        label,
+                        style: TextStyle(),
+                      ),
+                    ),
+                    Container(
+                      child: TextField(
+                      obscureText: checkPass,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
 }
